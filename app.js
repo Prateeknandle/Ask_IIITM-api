d776ab217cf0e063f98410052f8ea6250873a9a3
Prateek Nandle <prateeknandle@gmail.com>
2021-11-22 12:30:32 +0530

    update

--- app.js
+++ app.js
@@ -9,19 +9,10 @@ require("./db/connection");
 app.use(express.json());
 const User = require("./models/UserSchema");
 app.use(cookieParser(process.env.SECRET_KEY));
-<<<<<<< HEAD
-app.use(require('./router/auth'))
-app.use(require('./router/post'))
-app.use(require('./router/questions'))
-app.use(require('./router/opp'))
-
-
-=======
 app.use(require("./router/auth"));
 app.use(require("./router/post"));
 app.use(require("./router/questions"));
 app.use(require("./router/opp"));
->>>>>>> refs/remotes/origin/master
 
 const PORT = process.env.PORT;
 
