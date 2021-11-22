585f550512dbdf4c139a1b1fb21db046ba1471e8
Sarthakjain211001 <56023046+Sarthakjain211001@users.noreply.github.com>
2021-11-22 20:32:03 +0530

    Connecting Qna with backend

--- router/questions.js
+++ router/questions.js
@@ -7,7 +7,7 @@ const Question = require('../models/QuestionSchema');
 
 router.get('/questions',authenticate,(req,res)=>{
     Question.find()
-    .populate("postedBy","_id username")
+    .populate("postedBy","_id name")
     .then(questions=>{
         res.json(questions)
     }).catch(err=>{
@@ -20,7 +20,7 @@ router.post('/writequestion',authenticate,async(req,res)=>{
     if(!qtitle||!descofq){
         return res.status(422).json({error:"Fill all the fields"})
     }
-    const question = new Question({qtitle,descofq})
+    const question = new Question({qtitle,descofq,postedBy:req.rootUser})
     await question.save();
     res.status(201).json({message:"done"});
 })
@@ -41,7 +41,7 @@ router.put('/editquestion',authenticate,(req,res)=>{
 
     router.post('/answer/:id',authenticate,(req,res)=>{
     Question.findById(req.params.id).then(question=>{
-        question.answers.push({ answer: req.body.answer,postedBy:req.rootUser._id});
+        question.answers.push({ answer: req.body.answer,postedBy:req.rootUser});
          question.save()
          .then(result=>{
             res.json(result)
@@ -71,5 +71,74 @@ router.delete('/deletequestion/:id',authenticate,(req,res)=>{
         }
     })
 })
+
+router.get('/question/:id',authenticate,(req,res)=>{
+    Question.findById(req.params.id)
+    .populate("postedBy","_id name")
+    .populate("answers.postedBy","_id name")
+    .then(ques=>{
+        res.json(ques)
+    }).catch(err=>{
+        res.status(400).send(err);
+    })
+})
+
+// router.put('/answer/like',authenticate,(req,res)=>{
+//     Question.answers.answer.findByIdAndUpdate(req.body.ansId,{
+//         $push:{likes:req.rootUser._id}
+//     },{
+//         new:true
+//     }).exec((err,result)=>{
+//         if(err){
+//             return res.status(422).json({errror:err})
+//         }else{
+//             res.json(result)
+//         }
+//     })
+// })
+// router.put('/answer/like',authenticate,(req,res)=>{
+//     Question.findById(req.body.quesId).then(question=>{
+//         question.answers.findByIdAndUpdate(req.body.ansId,{
+//             $push:{likes:req.rootUser._id}
+//         },{
+//             new:true
+//          })
+// }).then(result=>{
+//     res.json(result)
+// }).catch(err=>{
+//     res.status(400).send(err);
+// })
+// })
+
+// router.put('/answer/unlike',authenticate,(req,res)=>{
+//     Question.findById(req.body.quesId).then(question=>{
+//         question.answers.findByIdAndUpdate(req.body.ansId,{
+//             $pull:{likes:req.rootUser._id}
+//         },{
+//             new:true
+//         }).exec((err,result)=>{
+//             if(err){
+//                 return res.status(422).json({errror:err})
+//             }else{
+//                 res.json(result)
+//             }
+//         })
+         
+// })
+// })
+// router.put('/answer/unlike',authenticate,(req,res)=>{
+//     Question.answers.answer.findByIdAndUpdate(req.body.ansId,{
+//         $pull:{likes:req.rootUser._id}
+//     },{
+//         new:true
+//     })
+//     .exec((err,result)=>{
+//         if(err){
+//             return res.status(422).json({errror:err})
+//         }else{
+//             res.json(result)
+//         }
+//     })
+// })
 
 module.exports = router;
